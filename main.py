7b5c9e9cbe41db8577071b19613979c18739d5b6
ShayCohenn <shay91847@gmail.com>
2023-10-18 22:03:45 +0300

    final QR test

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 from fastapi import FastAPI
 import segno
-from io import BytesIO
 import base64
-import tempfile  # Import the tempfile module
-import uvicorn
+import tempfile
 
 app = FastAPI()
 
 @app.get("/generate_qr")
-def generate_qr_code(data: str, back_color: str = "white", front_color: str = "black"):
+def generate_qr_code(data: str, back_color: str = "white", front_color: str = "black", scale:int = 20, border_size:int = 1,border_color:str = "white"):
     # Create a temporary file
     with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
         # Create a QR code and save it to the temporary file
         qr = segno.make_qr(data)
-        qr.save(temp_file.name, scale=20, border=1, light=back_color, dark=front_color,)
+        qr.save(temp_file.name, scale=scale, border=border_size, light=back_color, dark=front_color,quiet_zone=border_color)
 
     # Read the content of the temporary file
     with open(temp_file.name, "rb") as file:
         img_base64 = base64.b64encode(file.read()).decode()
 
     return {"QR_URL": f"data:image/png;base64,{img_base64}"}
-
-if __name__ == "__main":
-  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
