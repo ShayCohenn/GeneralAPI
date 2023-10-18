6171952782bc2095eb38b077750688b4c09ac8c0
ShayCohenn <shay91847@gmail.com>
2023-10-18 22:22:31 +0300

    QR done

---
+++ QR/qr.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+import tempfile
+import segno
+import base64
+
+router = APIRouter()
+
+@router.get("/generate")
+def generate_qr_code(data: str, back_color: str = "white", front_color: str = "black", scale:int = 20, border_size:int = 1,border_color:str = "white"):
+    # Create a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
+        # Create a QR code and save it to the temporary file
+        qr = segno.make_qr(data)
+        qr.save(temp_file.name, scale=scale, border=border_size, light=back_color, dark=front_color,quiet_zone=border_color)
+
+    # Read the content of the temporary file
+    with open(temp_file.name, "rb") as file:
+        img_base64 = base64.b64encode(file.read()).decode()
+
+    return {"QR_URL": f"data:image/png;base64,{img_base64}"}
--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 from fastapi import FastAPI
-import segno
-import base64
-import tempfile
+from QR.qr import router as qr_router
 
 app = FastAPI()
 
-@app.get("/generate_qr")
-def generate_qr_code(data: str, back_color: str = "white", front_color: str = "black", scale:int = 20, border_size:int = 1,border_color:str = "white"):
-    # Create a temporary file
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
-        # Create a QR code and save it to the temporary file
-        qr = segno.make_qr(data)
-        qr.save(temp_file.name, scale=scale, border=border_size, light=back_color, dark=front_color,quiet_zone=border_color)
-
-    # Read the content of the temporary file
-    with open(temp_file.name, "rb") as file:
-        img_base64 = base64.b64encode(file.read()).decode()
-
-    return {"QR_URL": f"data:image/png;base64,{img_base64}"}
+app.include_router(qr_router, prefix="/qr")
