d6c368b75a47e50fbdee42022bb4fb4b722f05ae
ShayCohenn <shay91847@gmail.com>
2023-10-21 23:48:23 +0300

    cors

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from QR.qr import router as qr_router
 from finance.endpoints import router as stocks_router
 from entertainment.endpoints import router as jokes_router
 
 app = FastAPI()
+
+# Enable CORS for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You should specify the allowed origins instead of "*"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(qr_router, prefix="/qr")
 app.include_router(stocks_router, prefix="/finance")
