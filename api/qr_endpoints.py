60d3f6ad5f95ab033de9479bab71591484494a92
ShayCohenn <shay91847@gmail.com>
2024-06-05 14:21:20 +0300

    qr url encoding bug fix - turned to POST from GET

--- api/qr_endpoints.py
+++ api/qr_endpoints.py
@@ -1,25 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 import tempfile
 import segno
 import base64
-from constants import MAIN_ERROR_MESSAGE
 
 router = APIRouter()
 
-@router.get("/generate")
-def generate_qr_code(data: str, back_color: str = "white", front_color: str = "black", scale:int = 20, border_size:int = 1, border_color:str = "white"):
+class QrParams(BaseModel):
+    data: str
+    back_color: str = "white"
+    front_color: str = "black"
+    scale:int = 20
+    border_size:int = 1
+    border_color:str = "white"
+
+@router.post("/generate")
+def generate_qr_code(qr: QrParams):
     try:
         # Create a temporary file
         with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
             # Create a QR code and save it to the temporary file
-            qr = segno.make_qr(data)
-            qr.save(temp_file.name, scale=scale, border=border_size, light=back_color, dark=front_color, quiet_zone=border_color)
+            qr = segno.make_qr(qr.data)
+            qr.save(temp_file.name, scale=qr.scale, border=qr.border_size, light=qr.back_color, dark=qr.front_color, quiet_zone=qr.border_color)
 
         # Read the content of the temporary file
         with open(temp_file.name, "rb") as file:
             img_base64 = base64.b64encode(file.read()).decode()
 
-        return {"QR_URL": f"data:image/png;base64,{img_base64}"}
-    except:
-        return JSONResponse(status_code=500, content=MAIN_ERROR_MESSAGE)
+        return JSONResponse(status_code=200, content={"QR_URL": f"data:image/png;base64,{img_base64}"})
+    except Exception as e:
+        raise HTTPException(detail=e, status_code=500)
