473f9ea17b9b8827e872a2c411afa3740229b9bb
ShayCohenn <shay91847@gmail.com>
2024-06-27 22:33:34 +0300

    changed geo classes to dataclasses

--- api/v1/geo/models.py
+++ api/v1/geo/models.py
@@ -1,23 +1,24 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
 class City:
-    def __init__(self, city: str, country: str, flag: str = None, dial_code: str = None, emoji: str = None):
-        self.city = city
-        self.country = country
-        self.flag = flag
-        self.dial_code = dial_code
-        self.emoji = emoji
+    city: str
+    country: str
+    flag: str = None
+    dial_code: str = None
+    emoji: str = None
 
     def __iter__(self):
         for attr, value in self.__dict__.items():
             if value is not None:
                 yield attr, value
 
-    
+@dataclass(frozen=True)
 class Country:
-    def __init__(self, country: str, flag: str = None, dial_code: str = None, emoji: str = None):
-        self.country = country
-        self.flag = flag
-        self.dial_code = dial_code
-        self.emoji = emoji
+    country: str
+    flag: str = None
+    dial_code: str = None
+    emoji: str = None
 
     def __iter__(self):
         for attr, value in self.__dict__.items():
