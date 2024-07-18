8959e965d62acdc8e0e56ca89c0b64b0ab0a06f6
ShayCohenn <shay91847@gmail.com>
2024-07-18 15:03:11 +0300

    cors

--- API/main.py
+++ API/main.py
@@ -27,6 +27,15 @@ class CustomCORSMiddleware(BaseHTTPMiddleware):
 
 app.add_middleware(CustomCORSMiddleware)
 
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 @app.exception_handler(404)
 async def custom_404_handler(_, __):
     return ORJSONResponse(status_code=404, content=Messages.MAIN_404_MESSAGE)
