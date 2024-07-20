9d6f9d762fc775af4bb42b6dbb43a4656cf934ee
ShayCohenn <shay91847@gmail.com>
2024-07-20 17:40:11 +0300

    auth ready for deployment

--- Auth/core/config.py
+++ Auth/core/config.py
@@ -4,21 +4,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Setup:
-    MONGODB_URI = getenv("MONGODB_URI")
-    REDIS_URL = getenv('REDIS_URL')
-    REDIS_PORT = int(getenv('REDIS_PORT'))
-    REDIS_PASSWORD = getenv('REDIS_PASSWORD')
-    GOOGLE_ID = getenv('GOOGLE_CLIENT_ID')
-    GOOGLE_SECRET = getenv('GOOGLE_CLIENT_SECRET')
-    SECRET_KEY = getenv('SECRET_KEY')
-    ALGORITHM = getenv('ALGORITHM')
+    MONGODB_URI: str = getenv("MONGODB_URI")
+    REDIS_URL: str = getenv('REDIS_URL')
+    REDIS_PORT: int = int(getenv('REDIS_PORT'))
+    REDIS_PASSWORD: str = getenv('REDIS_PASSWORD')
+    GOOGLE_ID: str = getenv('GOOGLE_CLIENT_ID')
+    GOOGLE_SECRET: str = getenv('GOOGLE_CLIENT_SECRET')
+    SECRET_KEY: str = getenv('SECRET_KEY')
+    ALGORITHM: str = getenv('ALGORITHM')
 
 class EmailConfig:
-    FROM_EMAIL = getenv('FROM_EMAIL')
-    PASSWORD = getenv('PASSWORD')
+    FROM_EMAIL: str = getenv('FROM_EMAIL')
+    PASSWORD: str = getenv('PASSWORD')
 
 class AppConfig:
-    MODE = getenv('MODE')
+    MODE: str = getenv('MODE')
+
+class Messages:
+    MAIN_ERROR_MESSAGE: str = {"error":"an error has occured please try again later, if this error persists please contact 'shay91847@gmail.com'"}
+    MAIN_404_MESSAGE: str = {"error":"Could not find this endpoint. Visit our documentation website at /docs"}
 
 # -------------------------------------------------------------------------- URLS -------------------------------------------------------------------
 
@@ -26,5 +30,5 @@ _FRONTEND_LOCAL_URL = "http://127.0.0.1:3000"
 _FRONTEND_PRODUCTION_URL = "https://general-api.vercel.app/"
 
 class URLS:
-    FRONTEND_URL = _FRONTEND_PRODUCTION_URL if AppConfig.MODE == "production" else _FRONTEND_LOCAL_URL
-    GOOGLE_REDIRECT_URI = f"{FRONTEND_URL}/auth/google"
+    FRONTEND_URL: str = _FRONTEND_PRODUCTION_URL if AppConfig.MODE == "production" else _FRONTEND_LOCAL_URL
+    GOOGLE_REDIRECT_URI: str = f"{FRONTEND_URL}/auth/google"
--- Auth/main.py
+++ Auth/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from auth.endpoints import router
+from core.config import URLS, Messages
+from fastapi.responses import ORJSONResponse
 
 # ----------------------------------------------- App Initialization ----------------------------------------------------------------------
 
@@ -11,19 +13,19 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[URLS.FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# @app.exception_handler(404)
-# async def custom_404_handler(_, __):
-#     return ORJSONResponse(status_code=404, content=Messages.MAIN_404_MESSAGE)
+@app.exception_handler(404)
+async def custom_404_handler(_, __):
+    return ORJSONResponse(status_code=404, content=Messages.MAIN_404_MESSAGE)
 
-# @app.exception_handler(500)
-# async def custom_500_handler(_, __):
-#     return ORJSONResponse(status_code=500, content=Messages.MAIN_ERROR_MESSAGE)
+@app.exception_handler(500)
+async def custom_500_handler(_, __):
+    return ORJSONResponse(status_code=500, content=Messages.MAIN_ERROR_MESSAGE)
 
 # ----------------------------------------------- Including The Routers -------------------------------------------------------------------
 
