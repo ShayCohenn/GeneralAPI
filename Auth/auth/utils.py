6261169255fb4c09643db56ff36460d286d4be74
ShayCohenn <shay91847@gmail.com>
2024-07-23 00:30:05 +0300

    unverified account deleted too soon bug - fixed

--- Auth/auth/utils.py
+++ Auth/auth/utils.py
@@ -49,12 +49,13 @@ async def remove_expired() -> None:
     """Remove expired unverified users and expired reset password tokens"""
     expiration_time = timedelta(minutes=15)
     while True:
+        current_time = datetime.now(timezone.utc)
         # Remove expired reset password tokens
-        expired_tokens = users_db.find({"reset_token_created_at": {"$lt": datetime.now() - expiration_time}})
+        expired_tokens = users_db.find({"reset_token_created_at": {"$lt": current_time - expiration_time}})
         for token in expired_tokens:
             users_db.update_one({"_id": token["_id"]}, {"$unset": {"reset_token": "", "reset_token_created_at": ""}})
         # Remove unverified users
-        expired_unverified_users = users_db.find({"created_at": {"$lt": datetime.now() - expiration_time}}, {"verified":False})
+        expired_unverified_users = users_db.find({"created_at": {"$lt": current_time - expiration_time, "verified": False}})
         for user in expired_unverified_users:
             users_db.delete_one({"_id": user["_id"], "verified": False})
         await asyncio.sleep(60)
