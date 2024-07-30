034a82002617a8802c5670a692a88171afd73221
ShayCohenn <shay91847@gmail.com>
2024-07-30 14:03:42 +0300

    background remove expired fix

--- Auth/auth/utils.py
+++ Auth/auth/utils.py
@@ -53,12 +53,17 @@ async def remove_expired() -> None:
         # Remove expired reset password tokens
         expired_tokens = users_db.find({"reset_token_created_at": {"$lt": current_time - expiration_time}})
         for token in expired_tokens:
-            users_db.update_one({"_id": token["_id"]}, {"$unset": {"reset_token": "", "reset_token_created_at": ""}})
+            await users_db.update_one({"_id": token["_id"]}, {"$unset": {"reset_token": "", "reset_token_created_at": ""}})
+        
         # Remove unverified users
-        expired_unverified_users = users_db.find({"created_at": {"$lt": current_time - expiration_time, "verified": False}})
+        expired_unverified_users = users_db.find({
+            "$and": [
+                {"created_at": {"$lt": current_time - expiration_time}},
+                {"verified": False}
+            ]
+        })
         for user in expired_unverified_users:
-            users_db.delete_one({"_id": user["_id"], "verified": False})
-        await asyncio.sleep(60)
+            await users_db.delete_one({"_id": user["_id"], "verified": False})
 
 
 def get_user(search_field: UserSearchField, query: str) -> Optional[User]:
